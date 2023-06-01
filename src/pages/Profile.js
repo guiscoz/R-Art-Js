76a440fe87be5dff16feba15553a0d5238b343b4
guiscoz <gui.x.scoz@gmail.com>
2023-05-31 23:16:18 -0300

    adicao de imagem

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Image } from "react-bootstrap";
+import "./Profile.css";
 
 const Profile = ({ user, followers, following, posts }) => {
-  return (
-    <>
-        <header>
-            <div className="picture"><Image id="avatar" src={`../db/${user.profile_picture}`}/></div>
-            <section>
-                <div className="top">{user.name}</div>
-                <div className="desc">{user.description}</div>
-                <div className="status">{posts.length} posts, {followers.length} followers, following {following.length}</div>
-            </section>
-        </header>
-    </>
-  );
+    return (
+        <>
+            <header>
+                <div className="picture"><Image id="avatar" alt="User Profile Picture" src={require(`../db/${user.profile_picture}`)}/></div>
+                <section>
+                    <div className="top">{user.name}</div>
+                    <div className="desc">{user.description}</div>
+                    <div className="status">{posts.length} posts, {followers.length} followers, following {following.length}</div>
+                </section>
+            </header>
+        </>
+    );
 };
 
 export default Profile;
